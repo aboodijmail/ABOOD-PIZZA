7cde07df624a1ec4d9d4bfc3c43555241c154c08
abed <aboodhih@yahoo.com>
2024-12-31 21:33:17 +0300

    4

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,14 +1,24 @@
-import { useDispatch } from "react-redux";
-import Button from "../../ui/Button";
-import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
-
-export default function UpdateItemQuantity({pizzaId}) {
-    const dispatch=useDispatch();
+export default function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
   return (
-    <div className="flex gap-1 items-center md:gap-3">
-        <Button onClick={()=>dispatch(decreaseItemQuantity(pizzaId))} type='round'>-</Button>
-        <Button onClick={()=>dispatch(increaseItemQuantity(pizzaId))} type='round'>+</Button>
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+        type="round"
+      >
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+        type="round"
+      >
+        +
+      </Button>
     </div>
-  )
+  );
 }
