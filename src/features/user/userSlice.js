ee4215c31b7b55d96779e40452688966f938ff6d
abed <aboodhih@yahoo.com>
2025-01-01 01:14:48 +0300

    5

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,7 +22,7 @@ import { createSlice } from "@reduxjs/toolkit"
 //   return { position, address };
 // }
 const initialState={
-  username: '',
+  username: "",
 }
 const userslice=createSlice({
   name:'user',
